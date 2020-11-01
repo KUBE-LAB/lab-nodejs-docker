6d848b99a0a180c804172bd24989231a1ccb73bb
Richard.Park <richard-park@RichardParkui-MacBook-Pro.local>
2020-11-01 18:56:32 +0900

    runtime

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ var http = require('http');
 var handleRequest = function(request, response) {
   console.log('Received request for URL: ' + request.url);
   response.writeHead(200);
-  response.end('Hello World!!! v1.1');
+  response.end('Chan and Ju, Go to School! v1.0');
 };
 var www = http.createServer(handleRequest);
 www.listen(8082);
